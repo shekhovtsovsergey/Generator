d7714511ccbcd94d2f34e2efa482d2bfa29ab4ab
shekhovtsovsergey <89629417575@mail.ru>
2025-07-16 18:11:09 +0300

    Generated random Java files

--- your_github_project_path/src/main/java/RandomClass_6.java
+++ your_github_project_path/src/main/java/RandomClass_6.java
@@ -1,5 +1,9 @@
 public class RandomClass_6 {
     public static void main(String[] args) {
         System.out.println("Random line!");
+        System.out.println("Random line!");
+        System.out.println("Random line!");
+        System.out.println("Random line!");
+        System.out.println("Random line!");
     }
 }
